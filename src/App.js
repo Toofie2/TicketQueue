3d73c088e1c2b12800b77c88d50e1ac4a54adede
Tiffani <ttiplee2@aol.com>
2026-07-10 09:36:28 -0500

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom'; 
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
-import History from './pages/History';
 import './styles/Login.css';
 
 function App() {
